f73c04294222bf90b32ffbe1e4f69a68c69d2082
Evgeny <xonix75@mail.ru>
2019-07-17 09:56:41 +0300

    DB

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Todo;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use \Symfony\Component\HttpFoundation\Response;
@@ -9,10 +10,21 @@ use \Symfony\Component\HttpFoundation\Response;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/", name="main")
      */
     public function index()
     {
+        $todo = new  Todo;
+        $em = $this->getDoctrine()->getManager();
+
+        $todo->setStatus('В процессе')
+            ->setPriority('Высокий')
+            ->setName('Первый todo')
+            ->setDateCreation(new \DateTime());
+
+        $em->persist($todo);
+        $em->flush();
+
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path'    => 'src/Controller/MainController.php',
